8de0f59ba825b6fc9d28b3d2a0167157012c63de
devsahadat <cse.devsahadat@gmail.com>
2023-05-21 12:12:58 +0600

    mongoose install and server setup

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
+const dotenv = require("dotenv");
+const colors = require("colors");
+const connectDb = require("./config/connectDb");
+// config dot env file
+dotenv.config();
+// database Call
+connectDb();
+//rest object
+const app = express();
+//middlewares
+app.use(cors());
+app.use(express.json());
+app.use(morgan("dev"));
+
+// routes
+app.get("/", (req, res) => {
+  res.send(`<h1>Hello From The Server</h1>`);
+});
+
+// port
+const PORT = 8080 || process.env.PORT;
+
+//listen server
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
